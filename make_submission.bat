14233e372432d5f2f33b0fe47939a1f82bedbcad
Dylan Asmar <asmar@stanford.edu>
2024-04-14 22:24:55 -0700

    use powershell for zipping vs 7zip

--- make_submission.bat
+++ make_submission.bat
@@ -1,13 +1,28 @@
 @echo off
-set project="project0"
-set py_name="%project%_py"
-set jl_name="%project%_jl"
-set zip_name="%project%.zip"
+setlocal
+
+set project=project0
+set py_name=%project%_py
+set jl_name=%project%_jl
+set zip_name=%project%.zip
+
+:: Read the language from language.txt
 set /p lang=<language.txt
+
+:: Determine the directory and file extension based on the language
 if "%lang%" == "julia" (
-    7z a -r %zip_name% language.txt %jl_name%/*.jl
+    set dir_name=%jl_name%
+    set ext=jl
 ) else if "%lang%" == "python" (
-    7z a -r %zip_name% language.txt %py_name%/*.py
+    set dir_name=%py_name%
+    set ext=py
 ) else (
     echo language.txt does not contain a valid language. Make sure it says either julia or python, and nothing else.
+    goto end
 )
+
+:: Use PowerShell to zip the files
+powershell -command "Get-ChildItem -Path '.\%dir_name%\' -Filter *.%ext% | Compress-Archive -DestinationPath '.\%zip_name%' -Force"
+
+:end
+endlocal
