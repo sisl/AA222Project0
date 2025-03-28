45f67bb9e6d10de2b73f10cab84266dbd45520a6
Michelle Ho <mtho@stanford.edu>
2025-03-27 22:30:13 -0700

    added contour plotting to project 0

--- project0_jl/project0.jl
+++ project0_jl/project0.jl
@@ -1,10 +1,34 @@
-## top-level submission file
-
+using Plots # Make sure you add this to your project dependencies
 
 """
-    f(a, b) = c
-A function that adds the inputs `a` and `b`
+    f(a, b)
+
+A function that adds the inputs `a` and `b`.
 """
 function f(a, b)
-    # write your code here. Remember to return the sum of a and b!
+    # TODO: Write your code here: Remember to return the sum of a and b!
 end
+
+# TODO: write ranges for a and b (you can adjust the limits and step sizes as needed)
+
+bottom_limit = -10
+top_limit = 10
+length = 100
+a_vals = # TODO: Define the range for a  
+b_vals = # TODO: Define the range for b  
+
+# Generates a 2D grid of function values
+z = @. f(a_vals', b_vals)
+
+# Use the computed arrays a_vals, b_vals, and z.
+p = contour(   # TODO: Replace with the appropriate arguments - see https://docs.juliaplots.org/latest/series_types/contour/
+        ,
+        ,
+        ,
+    title = "Contour Plot of f(a, b)",
+    xlabel = "a",
+    ylabel = "b"
+)
+
+# show plot
+display(p)
--- project0_py/project0.py
+++ project0_py/project0.py
@@ -7,8 +7,9 @@ from project0_py.helpers import *
 ## top-level submission file
 
 
-### imports here
+### imports here - TODO: remember to add them to your environment 
 import numpy as np
+import matplotlib.pyplot as plt
 ###
 
 
@@ -22,8 +23,30 @@ def f(a, b):
 		c (float): result
 	'''
 
-	## Your code goes here
+	## TODO: Fill your code in here 
 
 	##
 
 	return c
+
+# make contour plot 
+
+bottom_limit = -10
+top_limit = 10
+num_steps = 100
+a_vals = # TODO: use np.linspace to define range of values
+b_vals = # TODO: use np.linspace to define range of values
+
+# Generates a 2D grid of function values
+A, B = np.meshgrid(a_vals, b_vals)
+Z = f(A,B)
+
+# TODO: Generate a contour plot of the function values with the appropriate arguments.
+# See https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.contour.html for more information.
+plt.contourf( ,  ,  )  
+plt.title("Contour Plot of f(a, b)")
+plt.xlabel("a")
+plt.ylabel("b")
+
+# show plot
+plt.show()
